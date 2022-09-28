1eb858d63d8aad82293a3038601e53ab073ca658
S.alajlan <s.alajlan@salam.sa>
2022-09-28 16:59:31 +0300

    Test

--- helloworld/src/main/java/com/example/springboot/Application.java
+++ helloworld/src/main/java/com/example/springboot/Application.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
 
 public static void main(String[] args) {
-SpringApplication.run(DemoApplication.class, args);
+SpringApplication.run(Application.class, args);
 }
 
 @GetMapping("/hello")
